bf71e9f06aa158243f5a1cda4df0c43de0d0dfa9
Andrew <Andrew@andrews-mini.home>
2020-01-19 14:15:22 +0000

    Added new project

---
+++ GitPracTice3/GitPracTice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPracTice3
+//
+//  Created by Andrew on 19/01/2020.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
